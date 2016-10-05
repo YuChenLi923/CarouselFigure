4e10e2e7c8728eae62d7427d025ee84604946133
liyc1996 <swulll123@163.com>
2016-10-05 21:06:03 +0800

    更新至0.7

--- js/CarouselFigure.js
+++ js/CarouselFigure.js
@@ -1,70 +1,85 @@
-//轮播图 0.6 by chen
+//轮播图 0.7 by chen
 function Carousel(carouselInf){
 	this.inf=carouselInf;
 }
 Carousel.prototype.createCarousel=function(carouselInf){
-	console.log(carouselInf);
 	var dis,
-		carousel=document.getElementById(carouselInf.ID),
+		doc=document,
+		carousel=doc.getElementById(carouselInf.ID),
 		boxW=carousel.getElementsByTagName('li')[0].offsetWidth,
-		showNum=(document.getElementById(carouselInf.ID+'_show').offsetWidth)/boxW,
+		showBox=doc.getElementById(carouselInf.ID+'_show'),
+		showNum=(showBox.offsetWidth)/boxW,
 		tureNum=carousel.getElementsByTagName('li').length,
 		num=tureNum+showNum+1,
-		go=document.getElementById(carouselInf.ID+'_go'),
-		back=document.getElementById(carouselInf.ID+'_back'),
-		button=document.getElementById(carouselInf.ID+'_buttons'),	
+		go=doc.getElementById(carouselInf.ID+'_go'),
+		back=doc.getElementById(carouselInf.ID+'_back'),
+		button=doc.getElementById(carouselInf.ID+'_buttons'),	
 		time=carouselInf.time,
 		inteval=carouselInf.inteval,
 		index=carouselInf.index,
-		indexButton=carouselInf.indexButton;
+		indexButton=carouselInf.indexButton,
+		Time=carousel.getAttribute("AutoTime"),
+		auto=null;
 	carousel.style.width=num*boxW+'px';
 	carousel.style.left=-(index-1)*boxW+'px';
 	clone(showNum+1);
-		if(indexButton==true&&showNum==1){
-			var buttons=button.getElementsByTagName('span');
-			buttons[index-1].className='on';
+	if(indexButton==true&&showNum==1){
+		var buttons=button.getElementsByTagName('span');
+		buttons[index-1].className='on';
+	}
+	else{
+		button.style.cssText='display:none';
+	}
+	back.onclick=function(){
+		clickEvent(-1);
+	};
+	go.onclick=function(){
+		clickEvent(1);
+	};
+	if(Time!=0){
+		auto=setInterval(function(){
+			if(carousel.getAttribute("Animation")=='true'){
+				loop();
+			}
+		},Time*1000);
+		showBox.onmouseover=function(){
+			clearInterval(auto);
 		}
-		else{
-			button.style.cssText='display:none';
-		}
-		back.onclick=function(){
-			clickEvent(-1);
-		};
-		go.onclick=function(){
-			clickEvent(1);
-		};
-		if(carousel.getAttribute("AutoTime")!=0){
-				var Time=carousel.getAttribute("AutoTime");
-				dis=-boxW;
-				setInterval(function(){
-					if(carousel.getAttribute("Animation")=='true'){
-						if(indexButton==true&&showNum==1){
-							++index;
-							if(index>tureNum){
-								index=1;
-							}
-							addButtonOn(buttons);
-						}
-						carouselAnimation(dis);
-					}
-				},Time*1000);
-		}
-		if(indexButton==true&&showNum==1){
-			button.onclick=function(event){
+		showBox.onmouseout=function(){
+			auto=setInterval(function(){
 				if(carousel.getAttribute("Animation")=='true'){
-					var event=event?event:window.event;
-					var target=event.target;
-					var toIndex=target.getAttribute("index");
-					if(toIndex!=index&&toIndex){
-							dis=(toIndex-index)*boxW;
-							carousel.style.left=-1*boxW*(index-1);
-							Carousel.index=toIndex;
-							addButtonOn(buttons);
-							carouselAnimation(-1*dis);
-					}
+					loop();
 				}
-			};
-		}			
+			},Time*1000);
+		}
+	}
+	if(indexButton==true&&showNum==1){
+		button.onclick=function(event){
+			if(carousel.getAttribute("Animation")=='true'){
+				var event=event?event:window.event,
+					target=event.target,
+					toIndex=target.getAttribute("index"),
+					dis;
+				if(toIndex!=index&&toIndex){
+						dis=(toIndex-index)*boxW;
+						carousel.style.left=-1*boxW*(index-1);
+						index=toIndex;
+						addButtonOn(buttons);
+						carouselAnimation(-1*dis);
+				}
+			}
+		};
+	}
+	function loop(){
+		if(indexButton==true&&showNum==1){
+			++index;
+			if(index>tureNum){
+				index=1;
+			}
+				addButtonOn(buttons);
+		}
+		carouselAnimation(-boxW);
+	}			
 	function clickEvent(factor){
 			if(carousel.getAttribute("Animation")=='true'){
 					if(indexButton==true&&showNum==1){
@@ -83,26 +98,30 @@ Carousel.prototype.createCarousel=function(carouselInf){
 	}
 	function carouselAnimation(dis){
 		carousel.setAttribute("Animation",'false');
-		var speed=dis/(time*1000/inteval);
-		var left=parseInt(carousel.style.left);
-		var newpos=parseInt(carousel.style.left)+dis;	
+		var speed=dis/(time*1000/inteval),
+			left=parseInt(carousel.style.left),
+			newpos=left+dis,
+			preLeft=0;	
 		if(newpos>0&&dis>0){
 			carousel.style.left=((num-(showNum+1))*boxW*-1)+'px';
-			newpos=parseInt(carousel.style.left)+dis;
 			left=parseInt(carousel.style.left);
+			newpos=left+dis;
 		}
 		if(newpos<(boxW*(num-showNum+1)*(-1)+1)&&dis<0){
 			carousel.style.left=-1*boxW+'px';
-			newpos=parseInt(carousel.style.left)+dis;
 			left=parseInt(carousel.style.left);
+			newpos=left+dis;
 		}
+		preLeft=left;
 		var go = function(){
-			if(dis>0&&parseInt(carousel.style.left)<newpos || dis<0&&parseInt(carousel.style.left)>newpos){
-				carousel.style.left=parseInt(carousel.style.left)+speed+'px';
+
+			if(dis>0&&left<newpos || dis<0&&left>newpos){
+				carousel.style.left=left+speed+'px';
+				left=left+speed;
 				setTimeout(go,inteval);
 			}
 			else{
-				carousel.style.left=left+dis+'px';
+				carousel.style.left=preLeft+dis+'px';
 				carousel.setAttribute("Animation",'true');
 			}
 		}
